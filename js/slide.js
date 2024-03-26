bf2cfc61af030cea5c38a11c6fa03e8605f93a6c
Lucas <lucasmma10@gmail.com>
2024-03-26 10:46:47 -0300

    cauculo distancias quando ocorre os eventos

--- js/slide.js
+++ js/slide.js
@@ -2,6 +2,12 @@ export default class Slide {
   constructor() {
     this.slide = document.querySelector(".slide");
     this.wrapper = document.querySelector(".wrapper");
+    this.dist = {
+      finalPosition: 0,
+      distX: 0,
+      movement: 0,
+      movePosition: 0,
+    };
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
@@ -10,14 +16,22 @@ export default class Slide {
   onStart(event) {
     event.preventDefault();
     this.wrapper.addEventListener("mousemove", this.onMove);
+    this.dist.distX = event.pageX;
   }
 
-  onMove(event){
-
+  moveSlide(distX) {
+    this.slide.style.transform = "translate3d(" + distX + "px, 0px, 0px) ";
+    this.dist.movePosition = distX;
   }
 
-  onEnd(){
-    this.wrapper.removeEventListener("mousemove", this.onMove);
+  attPosicao(pageX) {
+    const posicaoFinal = (this.dist.distX - pageX) * 1.6;
+    return this.dist.finalPosition - posicaoFinal;
+  }
+
+  onMove(event) {
+    const posicao = this.attPosicao(event.pageX);
+    this.moveSlide(posicao);
   }
 
   addEventSlide() {
@@ -25,7 +39,12 @@ export default class Slide {
     this.wrapper.addEventListener("mouseup", this.onEnd);
   }
 
-  init(){
+  onEnd() {
+    this.wrapper.removeEventListener("mousemove", this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
+  }
+
+  init() {
     this.addEventSlide();
     return this;
   }
